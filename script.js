716b5cbc74bed0f977a8a96ab58c42a4599a0d32
Павел Рафеев <rafeev@office.bitrix.ru>
2017-01-30 16:25:16 +0200

    add addItem() method

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function VirtusKanbanGrid(options)
 {
     this.columns = Object.create(null);
+    this.items = Object.create(null);
     this.container = options.container;
     this.kanban = null;
     this.loadData(options);
@@ -11,6 +12,10 @@ VirtusKanbanGrid.prototype = {
     getColumn: function(columnId) {
         return this.columns[columnId] ? this.columns[columnId] : null;
     },
+
+    getItem: function(itemId) {
+		return this.items[itemId] ? this.items[itemId] : null;
+	},
 
     draw: function() {
         
@@ -39,10 +44,36 @@ VirtusKanbanGrid.prototype = {
 
     },
 
+    addItem: function(options) {
+
+        options = options || {};
+
+        if (this.getItem(options.id) !== null)
+		{
+			return;
+		}
+
+        var column = this.getColumn(options.columnId);
+
+        if (column)
+		{
+			var item = new VirtusKanbanItem(options);
+			item.kanban = this;
+
+			this.items[options.id] = item;
+			column.addItem(item);
+		}
+
+    },
+
     loadData: function(json) {
 
         json.columns.forEach(function(column) {
 			this.addColumn(column);
+		}, this);
+
+        json.items.forEach(function(item) {
+			this.addItem(item);
 		}, this);
 
     }
@@ -54,38 +85,160 @@ function VirtusKanbanColumn(options)
     this.name = options.name;
     this.items = [];
     this.layout = {
+        header: null,
         container: null,
         items: null,
-        name: null
+        name: null,
+        totalPrice: null,
+        count: null
     }
     this.virtusKanban = null;
 }
 
-
 VirtusKanbanColumn.prototype = {
 
-    createlayout: function() {
+    addItem: function(item, beforeItem) {
+		if (!item instanceof VirtusKanbanItem)
+		{
+			throw "item must be an instance of KanbanItem";
+		}
+
+		item.columnId = this.id;
+
+        var index = -1;
+
+        if(beforeItem)
+        {
+            for(var i = 0; i < this.items.length; i++)
+            {
+                if(this.items[i] == beforeItem)
+                {
+                    index = i;
+                }
+            }
+        }
+
+        if (index >= 0) 
+        {
+			this.items.splice(index, 0, item);
+		}
+		else
+		{
+			this.items.push(item);
+		}
+
+		if (this.layout.container)
+		{
+			this.render();
+
+        }
+	},
+
+    createLayout: function() {
 
         if (this.layout.container !== null)
         {
             return this.layout.container;
         }
 
+        // column layout
         this.layout.container = document.createElement('div');
-        this.layout.container.dataset.id = this.id;
-        this.layout.container.dataset.type = 'column';
+        this.layout.container.className = 'column';
+
+        // column header
+        this.layout.header = document.createElement('div');
+        this.layout.header.className = 'column__header';
+
+        // column title layout
+        this.layout.name = document.createElement('div');
+        this.layout.name.innerHTML = this.name;
+        this.layout.header.appendChild(this.layout.name);
+
+        // column items layout
+        this.layout.items = document.createElement('div');
+        this.layout.items.className = 'column__items'
+        this.layout.items.dataset.id = this.id;
+        this.layout.items.dataset.type = 'column';
+
+        // insert all div's in parent
+        this.layout.container.appendChild(this.layout.header);
+        this.layout.container.appendChild(this.layout.items);
 
         return this.layout.container;
+
     },
 
     render: function() {
 
-        this.createlayout();
+        if (this.layout.container === null)
+		{
+			this.createLayout();
+		}
+
+        for (var i = 0; i < this.items.length; i++)
+		{
+			var item = this.items[i];
+			this.layout.items.appendChild(item.render());
+		}
+
         return this.layout.container;
+
+    }
+}
+
+function VirtusKanbanItem(options)
+{
+    this.id = options.id;
+    this.name = options.name;
+    this.userId = options.userId;
+    this.price = options.price;
+    this.columnId = options.columnId;
+    this.layout = {
+        container: null,
+        info: null,
+        title: null,
+        price: null,
+        userAvatar: null
+    }
+}
+
+VirtusKanbanItem.prototype = {
+    
+    render: function() {
+        if (this.layout.container === null)
+        {
+            // item wrapper
+            this.layout.container = document.createElement('div');
+            this.layout.container.className = 'item';
+            this.layout.container.dataset.id = this.id;
+
+            // item title
+            this.layout.userAvatar = document.createElement('div');
+            this.layout.userAvatar.className = 'item__user';
+
+            // item title
+            this.layout.title = document.createElement('div');
+            this.layout.title.className = 'item__title';
+            this.layout.title.innerHTML = this.name;
+
+            // item price
+            this.layout.price = document.createElement('div');
+            this.layout.price.className = 'item__price';
+
+            // item info
+            this.layout.info = document.createElement('div');
+            this.layout.info.className = 'item__info';
+            this.layout.info.appendChild(this.layout.title);
+            this.layout.info.appendChild(this.layout.price);
+
+            this.layout.container.appendChild(this.layout.userAvatar);
+            this.layout.container.appendChild(this.layout.info);
+
+            return this.layout.container;
+        }
     }
 
 }
-
 
 var virtusKanban = new VirtusKanbanGrid(
     {
@@ -105,7 +258,8 @@ var virtusKanban = new VirtusKanbanGrid(
             {
                 id: 4,
                 name: "Development"
-            },{
+            },
+            {
                 id: 5,
                 name: "Testing"
             },
@@ -118,17 +272,17 @@ var virtusKanban = new VirtusKanbanGrid(
         items: [
             {
                 id: 1,
-                name: " ",
+                name: "Create Wordpress theme",
                 columnId: 1
             },
             {
                 id: 2,
-                name: " ",
+                name: "Fix bags",
                 columnId: 1
             },
             {
                 id: 3,
-                name: " ",
+                name: "Return to version 17.2",
                 columnId: 1
             }
         ],
